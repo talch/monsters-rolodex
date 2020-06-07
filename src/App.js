6b1dd256a3c5d11711ed4cb97d34244ea8b95daf
alchorny <alchorny@gmail.com>
2020-06-08 00:12:16 +0300

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchStr: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = e => this.setState({searchStr: e.target.value})
+
+  render() {
+    const {monsters, searchStr} = this.state;
+    const filteredMonsters = monsters.filter(m => 
+      m.name.toLowerCase().includes(searchStr.toLowerCase())
+      );
+
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='monster name'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card-list.styles.css';
+import { Card } from '../card/card.component';
+
+export const CardList = props => (
+    <div className="card-list">
+        {props.monsters.map(m => 
+            <Card key={m.id} monster={m}></Card>
+        )}
+    </div>
+);
